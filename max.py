4c4548845882c5b2aa793ebb2830c6547f88c19f
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-27 00:48:13 +0200

    Lambda needs further deeper learning/investigation

--- max.py
+++ max.py
@@ -1,6 +1,8 @@
-from typing import Iterable
+"""module docstring"""
+#from typing import Iterable
+import functools
 
-
-def max(l: Iterable[int]) -> int:
-
-    return 7
+def finding_max(initial_list):
+    """func docstring"""
+    maxxx = functools.reduce(lambda x, y: x if x > y else y, initial_list)
+    return maxxx
