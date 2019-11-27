f3f6ac05635a92d3ed0881b5dda92518a4f358f5
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-27 11:12:29 +0200

    Lambda needs further deeper learning/investigation

--- max.py
+++ max.py
@@ -2,7 +2,7 @@
 #from typing import Iterable
 import functools
 
-def finding_max(initial_list):
+
+def n_max(initial_list):
     """func docstring"""
-    maxxx = functools.reduce(lambda x, y: x if x > y else y, initial_list)
-    return maxxx
+    return functools.reduce(lambda x, y: x if x > y else y, initial_list)
