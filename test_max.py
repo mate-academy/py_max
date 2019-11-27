058a8991a403246294b9c51ff24e13f8b4aec685
alexander <earthhuman41@gmail.com>
2019-11-27 11:08:49 +0200

    implemented max

--- test_max.py
+++ test_max.py
@@ -1,13 +1,13 @@
-import max
+from max import max_
 
 
 def test_list():
-    assert max.max([2, 1, 5, 4, 7]) == 7
+    assert max_([2, 1, 5, 4, 7]) == 7
 
 
 def test_equal():
-    assert max.max([1, 1, 1, 1]) == 1
+    assert max_([1, 1, 1, 1]) == 1
 
 
 def test_empty():
-    assert max.max([1]) == 1
+    assert max_([1]) == 1
