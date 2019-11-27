b3af7687b059791f4fcf1c280afa3ce72613c2d9
logotip <logotip123@yahoo.com>
2019-11-27 12:12:59 +0200

    v 1.0

--- max.py
+++ max.py
@@ -1,6 +1,22 @@
+"""Search max digit module"""
 from typing import Iterable
+from functools import reduce
 
 
-def max(l: Iterable[int]) -> int:
-
-    return 7
+def max_(lst: Iterable[int]) -> int:
+    """
+    Search max digit with reduce method function
+    :param lst: list
+    :return: result
+    """
+    def maxfrom2(first, second):
+        """
+        func max analog for 2 arguments
+        :param first:
+        :param second:
+        :return: maximum digit
+        """
+        if first > second:
+            return first
+        return second
+    return reduce(maxfrom2, lst)
