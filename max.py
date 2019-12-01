4c5fc20dc775bf878de37e777da20360b1364a27
Platoniuks <platon@alterpost.org>
2019-12-01 11:58:14 +0200

    own implementation of built-in max()

--- max.py
+++ max.py
@@ -1,6 +1,9 @@
+"""Own implementation of built-in max() function using reduce()"""
+
 from typing import Iterable
+from functools import reduce
 
 
-def max(l: Iterable[int]) -> int:
-
-    return 7
+def max_(arr: Iterable[int]) -> int:
+    """Return the highest value"""
+    return reduce(lambda x, y: x if x > y else y, arr)
